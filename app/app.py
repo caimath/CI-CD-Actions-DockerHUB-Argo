c45c411a58e7b3c0adac1ee4255c1ae6dc41ef56
caimath <mathias.carsault@std.heh.be>
2025-12-16 23:31:37 +0100

    add kubernetes deployment.yml and service.yml and modify app.py
    modify app.py to show the pod and load balancer

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return f"Hello from {socket.gethostname()}"
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
